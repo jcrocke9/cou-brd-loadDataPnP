9e3956ab531d5b6fb7ed245c59ca4c2c9390292c
Joe Crockett <jcrocke9@hotmail.com>
2018-01-10 16:40:01 -0500

    further commands for finishing project

--- Add-CoaBoardsViews.ps1
+++ Add-CoaBoardsViews.ps1
@@ -1,10 +1,8 @@
 Import-Module SharePointPnPPowerShellOnline -NoClobber
 
-# List of Appointee Deleted
-Add-PnPView -List "Appointee" -Title "Deleted" -RowLimit 5000 -Fields "Title", "FirstName", "boardsCommi", "boardsArchive", "boardsDeletedDate", "boardsDeletedBy", "boardsDelete" -Query '<Where><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">1</Value></Eq></Where>'
-
-# Doesn't work - Names and Boards of All Archived
-# Add-PnPView -List "Appointee" -Title "Archived" -RowLimit 5000 -Fields "Title","FirstName","boardsCommi","boardsArchive" -Query '<Where><And><Eq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Eq><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq></And></Where>'
+# Site pages view for homepage
+Add-PnPView -List "Site Pages" -Title "Views" -RowLimit 30 -Fields "LinkFilenameNoMenu" -Query '<Where><And><Leq><FieldRef Name="SortOrder" /><Value Type="Number">12</Value></Leq><IsNotNull><FieldRef Name="SortOrder" /></IsNotNull></And></Where><OrderBy><FieldRef Name="SortOrder" Ascending="TRUE"/></OrderBy>'
+Add-PnPView -List "Site Pages" -Title "Reports" -RowLimit 30 -Fields "LinkFilenameNoMenu" -Query '<Where><And><Geq><FieldRef Name="SortOrder" /><Value Type="Number">13</Value></Geq><IsNotNull><FieldRef Name="SortOrder" /></IsNotNull></And></Where><OrderBy><FieldRef Name="SortOrder" Ascending="TRUE"/></OrderBy>'
 
 # 1 Appointments Expiring in a Given Period
 Add-PnPView -List "Appointee" -Title "ExpiringApp" -RowLimit 5000 -Fields "Title", "FirstName", "boardsCommi", "boardsArchive", "boardsEndDate", "boardsMemberType" -Query '<Where><And><Neq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Neq><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq></And></Where><OrderBy><FieldRef Name="boardsEndDate" Ascending="TRUE"/></OrderBy>'
@@ -31,3 +29,15 @@ Add-PnPView -List "Appointee" -Title "Email" -RowLimit 5000 -Fields "Title", "Fi
 
 # 10 List of Vacancies
 Add-PnPView -List "Appointee" -Title "Vacancies" -RowLimit 5000 -Fields "Title", "FirstName", "boardsCommi", "boardsMemberType" -Query '<Where><And><Neq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Neq><And><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq><Contains><FieldRef Name="Title"/><Value Type="Text">VACAN</Value></Contains></And></And></Where><OrderBy><FieldRef Name="boardsCommi" Ascending="TRUE"/></OrderBy>'
+
+# 11 List of Appointee Deleted
+Add-PnPView -List "Appointee" -Title "Deleted" -RowLimit 5000 -Fields "Title", "FirstName", "boardsCommi", "boardsArchive", "boardsDeletedDate", "boardsDeletedBy", "boardsDelete" -Query '<Where><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">1</Value></Eq></Where>'
+
+# 12 Doesn't work - Names and Boards of All Archived
+# Add-PnPView -List "Appointee" -Title "Archived" -RowLimit 5000 -Fields "Title","FirstName","boardsCommi","boardsArchive" -Query '<Where><And><Eq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Eq><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq></And></Where>'
+
+# 16 Committee's Public Roster
+Add-PnPView -List "Appointee" -Title "PublicRoster" -RowLimit 5000 -Fields "FirstName", "Title", "boardsMemberType", "boardsOriginalDate", "boardsStartDate", "boardsDateTaken", "boardsEndDate" -Query '<Where><And><Neq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Neq><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq></And></Where><OrderBy><FieldRef Name="Title" Ascending="TRUE"/></OrderBy>'
+
+# 17 Attempt #1 Members on Several Boards
+Add-PnPView -List "Appointee" -Title "SeveralBoards" -RowLimit 5000 -Fields "FirstName", "Title", "boardsMemberType", "boardsCommi" -Query '<Where><And><Neq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Neq><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq></And></Where><OrderBy><FieldRef Name="Title" Ascending="TRUE"/><FieldRef Name="FirstName" Ascending="TRUE"/><FieldRef Name="boardsCommi" Ascending="TRUE"/></OrderBy>'
---
+++ Dev.ps1
@@ -0,0 +1,18 @@
+function Add-CoaCommiRosterUrl {
+    Add-PnPFieldFromXml -FieldXml '<Field ID="b0966896-ed7b-43a4-8bb3-95b163419d3a" Name="commiRosterUrl" DisplayName="Roster" Type="URL" Group="Commissions"></Field>'
+    Add-PnPFieldToContentType -Field "commiRosterUrl" -ContentType Commission
+    Add-PnPView -List "Commissions" -Fields "commiRosterUrl" -Title "Roster" -RowLimit 300 -Query '<OrderBy><FieldRef Name="Title" Ascending="TRUE"/></OrderBy>'
+}
+
+function Set-CoaItemFieldCommi {
+    Get-PnPListItem -List "Commissions" -Fields "Title" | ForEach-Object {
+        $itemTitle = $_["Title"]
+        $itemId = $_.Id;
+        Set-PnPListItem -List "Commissions" -Identity $itemId -Values @{"commiRosterUrl" = "https://qa01alexandriava.sharepoint.com/sites/boardsqa/SitePages/Roster.aspx?commi=$itemTitle, $itemTitle"}
+        Clear-Variable itemId;
+        Clear-Variable itemTitle;   
+    }
+}
+
+Add-PnPFieldFromXml -List "Appointee" -FieldXml '<Field ID="6b7f2d60-684b-4862-8eec-dd5b3440a422" Name="boardsTerm" DisplayName="Years Served" Type="Calculated" Formula="=IF(OR(ISBLANK([boardsStartDate]),ISBLANK([boardsEndDate])),"",DATEDIF([boardsStartDate],[boardsEndDate],"D"))"></Field>'
+Add-PnPView -List "Appointee" -Title "YearsServed" -Fields "Title", "FirstName", "boardsCommi", "boardsOriginalDate", "boardsTerm", "boardsEndDate" -RowLimit 5000 -Query '<Where><And><Neq><FieldRef Name="boardsArchive" /><Value Type="Boolean">1</Value></Neq><Eq><FieldRef Name="boardsDelete" /><Value Type="Boolean">0</Value></Eq></And></Where>'
